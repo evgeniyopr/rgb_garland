7eb59c272d5f5ed083c1169bb316ac3f65ed3eff
Evgeniy Opryshko <>
2021-11-16 21:12:16 +0200

    init

---
+++ rgbGarland/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rgbGarland
+//
+//  Created by Evgeniy Opryshko on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
